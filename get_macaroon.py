ec5107b3d3dc43e0f5d8f4b8153b013eccb1ac68
Diego Davila <didavila@ucsd.edu>
2020-08-06 14:05:43 -0700

    adding a new version of get_macaroon.py that: is executbale, has argsparser, has configparser and shows macaroon deserialized

--- get_macaroon.py
+++ get_macaroon.py
@@ -1,29 +1,61 @@
-# This script is used to cook a curl command to get a checksum for a given url.
+#!/usr/bin/env python
 import sys
+import os
 import logging
+import ConfigParser
+import argparse
+from pymacaroons import Macaroon, Verifier
 from tpc_utils import *
-import pdb
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--verbose", "-v", help="Verbose", action="store_true")
+    parser.add_argument("--source", "-s", help="Source URL")
+    return parser.parse_args()
+
 
 def main():
-    #----- Config ----------------------------------------------------------------
-    curl_debug = 1
-    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s  %(levelname)s - %(message)s', datefmt='%Y%m%d %H:%M:%S')
-    
-    url = sys.argv[1]
-    new_filename = None
-    if len(sys.argv) == 3:
-        new_filename = sys.argv[2]
-        log.info("Downloading: "+url+" as: "+new_filename)
+    #---- Read arguments-------------------------------------------------------- 
+    args = parse_args()
+    url = args.source
+    if not "https" in url:
+        log.error("URL has to start with https")
+        sys.exit(1)
+    #---------------------------------------------------------------------------
+
+    #----- Config --------------------------------------------------------------
+    # Set the logging level
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG, format='%(asctime)s  %(levelname)s - %(message)s', datefmt='%Y%m%d %H:%M:%S')
     else:
-        log.info("Showing content of: "+url)
-    timeout = 120
-    proxy= "/tmp/x509up_u0"
-    #-------------------------------------------------------------------------------
-   
+        logging.basicConfig(level=logging.INFO, format='%(asctime)s  %(levelname)s - %(message)s', datefmt='%Y%m%d %H:%M:%S')
+    
+    # Check that the configuration file exists
+    if os.path.isfile(".config"):
+        configParser = ConfigParser.RawConfigParser()
+        configParser.read(".config")
+        
+        curl_debug = configParser.getint('all', 'curl_debug')
+        proxy      = configParser.get('all', 'proxy')
+        timeout    = configParser.getint('all', 'timeout')
+    else:
+        # If the .config file doesn't exist, set the defaults        
+        curl_debug = 1
+        proxy= "/tmp/x509up_u0"
+        timeout = 120
+
+    #---------------------------------------------------------------------------
+    
+  
     tpc_util = TPC_util(log, timeout, curl_debug, proxy)
-    macaroon = tpc_util.request_macaroon(url, "DOWNLOAD,DELETE,LIST")
-    #pdb.set_trace()
-    print(macaroon)
+    macaroon = tpc_util.request_macaroon(url, "DOWNLOAD,LIST")
+    if macaroon:
+        log.info("Macaroon: "+macaroon)
+        n = Macaroon.deserialize(macaroon)
+        log.debug("Macaroon deserialized:\n"+n.inspect())
+    else:
+        log.error("Cannot get the macaroon")
+
 
 log = logging.getLogger()    
 if __name__ == "__main__":
